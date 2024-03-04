260217903d4bd721e2cc36cb51161a9ce4e9a5e4
isaacherbozo <isaacherbozo06@gmail.com>
2024-03-04 14:07:30 +0100

    addTwoNumbers almost complete

--- AddTwoNumbers/main.c
+++ AddTwoNumbers/main.c
@@ -32,31 +32,55 @@ void readInReverse(ListNode* l1) {
     printf("\n");
 }
 
+int checkIfNextNull(ListNode* l1) {
+    return (l1->next == NULL) ? 1 : 0;
+}
+
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     ListNode* templ1 = l1;
     ListNode* templ2 = l2;
     ListNode* result = NULL;
 
-    while (l1 != NULL) {
+    int carry = 0;
+    while (templ1 != NULL) {
         int sum = templ1->val + templ2->val;
 
-        templ1 = (ListNode *) templ1->next;
-    }
+        if(sum < 10) {
+            result = addNode(result, sum + carry);
+            carry = 0;
+        } else {
+            if (checkIfNextNull(templ1) == 1) {
+                result = addNode(result, sum - 10 + carry);
+                result = addNode(result, 1);
+            } else {
+                result = addNode(result, sum - 10 + carry);
+                carry = 1;
+            }
+        }
 
+        templ1 = (ListNode *) templ1->next;
+        templ2 = (ListNode *) templ2->next;
+    }
+/*
+    while (templ2 != NULL){
+        result = addNode(result, templ2->val);
+        templ2 = (ListNode *) templ2->next;
+    }
+*/
 
     return result;
 }
 
 int main() {
     ListNode * l1 = NULL;
-    l1 = addNode(l1, 3);
-    l1 = addNode(l1, 4);
-    l1 = addNode(l1, 2);
+    l1 = addNode(l1, 9);
+    l1 = addNode(l1, 9);
+    l1 = addNode(l1, 9);
 
     ListNode * l2 = NULL;
-    l2 = addNode(l2, 4);
-    l2 = addNode(l2, 6);
-    l2 = addNode(l2, 5);
+    l2 = addNode(l2, 9);
+    l2 = addNode(l2, 9);
+    l2 = addNode(l2, 9);
 
     ListNode* result = addTwoNumbers(l1, l2);
 
